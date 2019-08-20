2c2c7f3ea5caa11c5074dbc530e2b7d134714029
Bashayer Nouri <bashayer_nouri@hotmail.com>
2019-08-20 14:55:46 +0300

    Manuel_Bashayer

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,11 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    "my_list": [ 
+    {"restaurant_name": "Babel", "food_type": "Lebanese",},
+    {"restaurant_name": "Mais Al Ghanim", "food_type": "Lebanese",},
+    {"restaurant_name": "Fridays", "food_type": "American",},
+    ],
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +19,13 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+
+    "my_object": {
+
+    "restaurant_name": "Babel", 
+    "food_type": "Lebanese",
+    
+    }, 
 
     }
     return render(request, 'detail.html', context)
